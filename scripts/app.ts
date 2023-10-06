a9cf07dd6a83d83d82c2754c826ad747a61cfe91
mamlaki <melekredwan@icloud.com>
2023-10-06 02:06:08 -0600

    General: Implemented and tested some basic logic. Will serve as a foundation going forward.

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,22 +1,63 @@
-const typingArea: HTMLTextAreaElement | null = document.querySelector('.user-input') as HTMLTextAreaElement
+const typingArea: HTMLElement | null = document.querySelector('.typing-area')
 const startBtn: HTMLElement | null = document.querySelector('.start-btn')
 
 const sampleTexts: string[] = ['Sample text 1', 'Sample text 2', 'Sample text 3']
-
-let isStarting = true
+let currentPosition: number = 0
+let currentPrompt: string = ''
 
 if (startBtn && typingArea) {
   startBtn.addEventListener('click', () => {
-    const randomText: string = sampleTexts[Math.floor(Math.random() * sampleTexts.length)]
-    typingArea.value = randomText
-
-    isStarting = true
+    currentPrompt = sampleTexts[Math.floor(Math.random() * sampleTexts.length)]
+    typingArea.textContent = currentPrompt
+    currentPosition = 0
+    setCursorAtStart(typingArea)
   })
 
-  typingArea.addEventListener('keydown', (e) => {
-    if (isStarting) {
-      typingArea.value = '';
-      isStarting = false
+  typingArea.addEventListener('input', (e) => {
+    const plainText = typingArea.textContent ? typingArea.textContent.replace(/<[^>]*>/g, "") : ""
+    
+    if (plainText[currentPosition] === currentPrompt[currentPosition]) {
+      applyCharacterStyle('correct')
+    } else {
+      applyCharacterStyle('incorrect')
     }
+
+    currentPosition++
   })
+}
+
+function applyCharacterStyle(className: string) {
+  if (typingArea) {
+    const charToStyle = currentPrompt[currentPosition]
+    const preText = currentPrompt.substring(0, currentPosition)
+    const postText = currentPrompt.substring(currentPosition + 1)
+    const styledChar = `<span class="${className}">${charToStyle}</span>`
+  
+    typingArea.innerHTML = preText + styledChar + postText
+    setCursorAfterStyledChar(typingArea, currentPosition)
+  }
+}
+
+function setCursorAtStart(element: HTMLElement) {
+  const range = document.createRange()
+  const sel = window.getSelection()
+  range.setStart(element, 0)
+  range.collapse(true)
+  sel?.removeAllRanges()
+  sel?.addRange(range)
+  element.focus()
+}
+
+function setCursorAfterStyledChar(element: HTMLElement, position: number) {
+  const range = document.createRange()
+  const sel = window.getSelection()
+
+  const styledSpan = element.querySelectorAll('span')[0]
+
+  if (styledSpan) {
+    range.setStartAfter(styledSpan)
+    range.collapse(true)
+    sel?.removeAllRanges()
+    sel?.addRange(range)
+  }
 }
--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,19 +1,55 @@
 "use strict";
-var typingArea = document.querySelector('.user-input');
+var typingArea = document.querySelector('.typing-area');
 var startBtn = document.querySelector('.start-btn');
 var sampleTexts = ['Sample text 1', 'Sample text 2', 'Sample text 3'];
-var isStarting = true;
+var currentPosition = 0;
+var currentPrompt = '';
 if (startBtn && typingArea) {
     startBtn.addEventListener('click', function () {
-        var randomText = sampleTexts[Math.floor(Math.random() * sampleTexts.length)];
-        typingArea.value = randomText;
-        isStarting = true;
+        currentPrompt = sampleTexts[Math.floor(Math.random() * sampleTexts.length)];
+        typingArea.textContent = currentPrompt;
+        currentPosition = 0;
+        setCursorAtStart(typingArea);
     });
-    typingArea.addEventListener('keydown', function (e) {
-        if (isStarting) {
-            typingArea.value = '';
-            isStarting = false;
+    typingArea.addEventListener('input', function (e) {
+        var plainText = typingArea.textContent ? typingArea.textContent.replace(/<[^>]*>/g, "") : "";
+        if (plainText[currentPosition] === currentPrompt[currentPosition]) {
+            applyCharacterStyle('correct');
         }
+        else {
+            applyCharacterStyle('incorrect');
+        }
+        currentPosition++;
     });
+}
+function applyCharacterStyle(className) {
+    if (typingArea) {
+        var charToStyle = currentPrompt[currentPosition];
+        var preText = currentPrompt.substring(0, currentPosition);
+        var postText = currentPrompt.substring(currentPosition + 1);
+        var styledChar = "<span class=\"".concat(className, "\">").concat(charToStyle, "</span>");
+        typingArea.innerHTML = preText + styledChar + postText;
+        setCursorAfterStyledChar(typingArea, currentPosition);
+    }
+}
+function setCursorAtStart(element) {
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(element, 0);
+    range.collapse(true);
+    sel === null || sel === void 0 ? void 0 : sel.removeAllRanges();
+    sel === null || sel === void 0 ? void 0 : sel.addRange(range);
+    element.focus();
+}
+function setCursorAfterStyledChar(element, position) {
+    var range = document.createRange();
+    var sel = window.getSelection();
+    var styledSpan = element.querySelectorAll('span')[0];
+    if (styledSpan) {
+        range.setStartAfter(styledSpan);
+        range.collapse(true);
+        sel === null || sel === void 0 ? void 0 : sel.removeAllRanges();
+        sel === null || sel === void 0 ? void 0 : sel.addRange(range);
+    }
 }
 //# sourceMappingURL=app.js.map
